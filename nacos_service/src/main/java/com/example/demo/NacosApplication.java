52478c3387366d0ab082b063b75ae648577ce126
gjw1990 <764567093@qq.com>
2023-08-26 07:52:13 +0800

    2222

--- nacos_service/src/main/java/com/example/demo/NacosApplication.java
+++ nacos_service/src/main/java/com/example/demo/NacosApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//启动
 @SpringBootApplication
 public class NacosApplication {
     public static void main(String[] args) {
